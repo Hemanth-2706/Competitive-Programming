e692af4f103dca0a3cbff9896f6229334f85fb8f
Hemanth M <ee24b024@smail.iitm.ac.in>
2026-01-24 23:29:18 +0530

    first upsolving learning...

---
+++ Upsolving/1.py
@@ -0,0 +1,24 @@
+"""
+https://codeforces.com/gym/664897/problem/N
+"""
+
+def solve():
+    n,m=map(int,input().split())
+    s = [int(x) for x in input()]
+    a=list(map(int,input().split()))
+    cum_sum = 0
+    for x in a:
+        cum_sum += x
+        if cum_sum >= 0:
+            continue
+        elif -cum_sum < len(s):
+            s = s[:len(s)+cum_sum]  # --->>> Avoid slicing lists inside loop. Insead keep track of how much to slice at the end.
+            cum_sum = 0
+        else:
+            return "0"
+    s = s + [0]*cum_sum
+    return ''.join(str(x) for x in s)
+
+t=int(input())
+for _ in range(t):
+    print(solve())
